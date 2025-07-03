0c946f4a7989d8525d3f8ec031d74fbfb22f9bac
jdelaney <jdelaney@wetdesign.com>
2025-07-03 02:08:33 -0700

    logger working nicely, now make more demos.

--- include/gaudi/logger.hpp
+++ include/gaudi/logger.hpp
@@ -8,12 +8,12 @@ namespace gaudi {
 namespace logger {
 
 void line(const vec3 &p0, const vec3 &p1, const vec4 &color);
-void lines(const std::vector<vec3> &p0, const std::vector<vec3> &p1,
-           const std::vector<vec4> &colors);
+//void lines(const std::vector<vec3> &p0, const std::vector<vec3> &p1,
+//           const std::vector<vec4> &colors);
 void box(const vec3 &cen, const vec3 &h, const vec4 &col);
 void ext(const vec3 &mn, const vec3 &mx, const vec4 &col);
-void field(const std::vector<vec3> &p, const std::vector<vec3> &dirs,
-           double D = 0.1);
+//void field(const std::vector<vec3> &p, const std::vector<vec3> &dirs,
+//           double D = 0.1);
 void frame(const mat3 &M, const vec3 &c, double C);
 void point(const vec3 &p0, const vec4 &color);
 void clear();
